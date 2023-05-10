399d6df782c1dd67ad5cdcdc857d8bb85b0b765e
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-05-10 12:31:59 -0400

    updated functionality

--- src/Components/Sneakers.js
+++ src/Components/Sneakers.js
@@ -11,16 +11,16 @@ const API = process.env.REACT_APP_API_URL;
 const Sneakers = () => {
   const [sneakers, setSneakers] = useState([]);
 
-//   useEffect(() => {
-//     axios
-//       .get(`${API}/sneakers`)
-//       .then((response) => {
-//         setSneakers(response.data);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }, []);
+  useEffect(() => {
+    axios
+      .get(`${API}/sneakers`)
+      .then((response) => {
+        setSneakers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div className="Sneakers">
