b12a8cec42ec0c364a032ba56204a565521a77b1
Marangely Rosas <Ladykaliber22@gmail.com>
2023-05-10 10:45:16 -0400

    app.js completed with routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+// DEPENDENCIES
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
+
+// PAGES
+import Edit from "./Pages/Edit";
+import FourOFour from "./Pages/FourOFour";
+import Home from "./Pages/Home";
+import Index from "./Pages/Index";
+import New from "./Pages/New";
+import Show from "./Pages/Show";
+
+// COMPONENTS
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sneakers" element={<Index />} />
+            <Route path="/sneakers/new" element={<New />} />
+            <Route exact path="/sneakers/:id" element={<Show />} />
+            <Route path="/sneakers/:id/edit" element={<Edit />} />
+            <Route path="*" element={<FourOFour />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
