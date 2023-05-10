75ad598505be107b6d9faa0d3d948bd82120c0b8
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-05-10 03:18:22 -0400

    Configured Sneaker.js and Sneakers.js added some css for Sneaker.js elements

--- src/Components/Sneakers.js
+++ src/Components/Sneakers.js
@@ -1,5 +1,41 @@
+//DEPENDENCIES
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+//FILE IMPORTS
+import Sneaker from "./Sneaker.js";
+
+//API
+const API = process.env.REACT_APP_API_URL;
+
 const Sneakers = () => {
-    return 'In Sneakers.js'
+  const [sneakers, setSneakers] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get(`${API}/sneakers`)
+//       .then((response) => {
+//         setSneakers(response.data);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+
+  return (
+    <div className="Sneakers">
+      <section className="Heading">
+        <h1>All Sneakers</h1>
+      </section>
+      <section className="Sneakers-List">
+        <ul>
+          {sneakers.map((sneaker) => {
+            return <Sneaker key={sneaker.id} sneaker={sneaker} />;
+          })}
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Sneakers;
