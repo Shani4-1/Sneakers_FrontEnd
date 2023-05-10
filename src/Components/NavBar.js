b8aaa095cd3ef0878998dc62da9db00886e0f5c2
Shaniqua Coston <shaniquacoston@pursuit.org>
2023-05-10 01:24:56 -0400

    imported Link and added necessary code missing routes

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-    return 'In the NavBar'
+  return (
+    <nav>
+      <h1>
+        <Link>👟Sneaker App👟</Link>
+      </h1>
+      <button>
+        <Link>All Sneakers</Link>
+      </button>
+      <button>
+        <Link>Add New Sneaker</Link>
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
